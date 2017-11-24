2b14468d00d420b0df31d25980468ef1128bcae6
Yusuke Yamada <yamachu.dev@gmail.com>
2017-11-24 11:34:19 +0900

    配列の受け取り修正して非同期化した

--- src/WorldWeb.js
+++ src/WorldWeb.js
@@ -60,49 +60,39 @@ export default class WorldWeb {
         return WorldWeb.functions.GetFFTSize(fs);
     }
 
-    static GetSpeechFeatures(x, fs, f0_length, fft_size) {
-        if (!WorldWeb.checkModuleEnabled()) return null;
-        if (f0_length === undefined) f0_length = WorldWeb.GetF0Length(x.length, fs);
-        if (fft_size === undefined) fft_size = WorldWeb.GetFFTSize(fs);
-
-        // X - input waveform
-        let pointer_x = WorldWeb.module._malloc(x.length * 8); // 64bit => 8byte
-        let offset_x = pointer_x / 8;
-        WorldWeb.module.HEAPF64.set(x, offset_x);
-
-        // F0
-        let pointer_f0 = WorldWeb.module._malloc(f0_length * 8);
-        let offset_f0 = pointer_f0 / 8;
-
-        // Spectrogram
-        let pointer_sp = WorldWeb.module._malloc((f0_length * (fft_size / 2 + 1)) * 8);
-        let offset_sp = pointer_sp / 8;
-
-        // F0
-        let pointer_ap = WorldWeb.module._malloc((f0_length * (fft_size / 2 + 1)) * 8);
-        let offset_ap = pointer_ap / 8;
-
-        WorldWeb.functions.GetSpeechFeatures(pointer_x, x.length, fs, pointer_f0, f0_length, pointer_sp, pointer_ap, fft_size);
-
-        // Heap to JS TypedArray
-        let f0 = new Float64Array(WorldWeb.module.HEAPF64.buffer, pointer_f0, f0_length);
-        let sp = new Float64Array(WorldWeb.module.HEAPF64.buffer, pointer_sp, f0_length * (fft_size / 2 + 1));
-        let ap = new Float64Array(WorldWeb.module.HEAPF64.buffer, pointer_ap, f0_length * (fft_size / 2 + 1));
-
-        // Free memory
-        WorldWeb.module._free(pointer_x);
-        WorldWeb.module._free(pointer_f0);
-        WorldWeb.module._free(pointer_sp);
-        WorldWeb.module._free(pointer_ap);
-
-        return {
-            fs: fs,
-            f0_length: f0_length,
-            fft_size: fft_size,
-            f0: f0,
-            sp: sp,
-            ap: ap
-        };
+    static GetSpeechFeaturesAsync(x, fs, f0_length, fft_size) {
+        return new Promise((resolve, reject) => {
+            if (!WorldWeb.checkModuleEnabled()) reject('WorldWeb is not initialized');
+            if (f0_length === undefined) f0_length = WorldWeb.GetF0Length(x.length, fs);
+            if (fft_size === undefined) fft_size = WorldWeb.GetFFTSize(fs);
+    
+            let pointer_x = WorldWeb.SetFloat64ArrayPointer(x);
+            let pointer_f0 = WorldWeb.GetFloat64ArrayPointer(f0_length);
+            let pointer_sp = WorldWeb.GetFloat64ArrayPointer(f0_length * (fft_size / 2 + 1));
+            let pointer_ap = WorldWeb.GetFloat64ArrayPointer(f0_length * (fft_size / 2 + 1));
+    
+            WorldWeb.functions.GetSpeechFeatures(pointer_x, x.length, fs, pointer_f0.pointer, f0_length, pointer_sp.pointer, pointer_ap.pointer, fft_size);
+    
+            // Heap to JS TypedArray
+            let f0 = WorldWeb.GetFloat64ArrayFromPointerWithOffset(pointer_f0, f0_length);
+            let sp = WorldWeb.GetFloat64ArrayFromPointerWithOffset(pointer_sp, f0_length * (fft_size / 2 + 1));
+            let ap = WorldWeb.GetFloat64ArrayFromPointerWithOffset(pointer_ap, f0_length * (fft_size / 2 + 1));
+    
+            // Free memory
+            WorldWeb.module._free(pointer_x);
+            WorldWeb.module._free(pointer_f0.pointer);
+            WorldWeb.module._free(pointer_sp.pointer);
+            WorldWeb.module._free(pointer_ap.pointer);
+    
+            resolve({
+                fs: fs,
+                f0_length: f0_length,
+                fft_size: fft_size,
+                f0: f0,
+                sp: sp,
+                ap: ap
+            });
+        });
     }
 
     static GetSynthesisFormLength(f0_length, fs) {
@@ -110,7 +100,53 @@ export default class WorldWeb {
         return WorldWeb.functions.GetSynthesisFormLength(f0_length, fs);
     }
 
-    static GetSynthesisForm(world_parameters) {
+    static GetSynthesisFormAsync(world_parameters) {
+        return new Promise((resolve, reject) => {
+            if (!WorldWeb.checkModuleEnabled()) reject('WorldWeb is not initialized');
+            if (f0_length === undefined) f0_length = WorldWeb.GetF0Length(x.length, fs);
+            if (fft_size === undefined) fft_size = WorldWeb.GetFFTSize(fs);
+    
+            let pointer_f0 = WorldWeb.SetFloat64ArrayPointer(world_parameters.f0);
+            let pointer_sp = WorldWeb.SetFloat64ArrayPointer(world_parameters.sp);
+            let pointer_ap = WorldWeb.SetFloat64ArrayPointer(world_parameters.ap);
 
+            let y_length = WorldWeb.GetSynthesisFormLength(world_parameters.f0_length, world_parameters.fs);
+            let pointer_y = WorldWeb.GetFloat64ArrayPointer(y_length);
+    
+            WorldWeb.functions.GetSynthesisForm(world_parameters.fs, pointer_f0, world_parameters.f0_length, pointer_sp.pointer, pointer_ap.pointer, world_parameters.fft_size, pointer_y.pointer, y_length);
+    
+            // Heap to JS TypedArray
+            let y = WorldWeb.GetFloat64ArrayFromPointerWithOffset(pointer_y, y_length);
+    
+            // Free memory
+            WorldWeb.module._free(pointer_y.pointer);
+            WorldWeb.module._free(pointer_f0);
+            WorldWeb.module._free(pointer_sp);
+            WorldWeb.module._free(pointer_ap);
+    
+            resolve(y);
+        });
+    }
+
+    static SetFloat64ArrayPointer(arr) {
+        let arr_pointer = WorldWeb.module._malloc(arr.length * 8);
+        let arr_offset = arr_pointer / 8;
+        WorldWeb.module.HEAPF64.set(arr, arr_offset);
+
+        return arr_pointer;
+    }
+
+    static GetFloat64ArrayPointer(length) {
+        let pointer = WorldWeb.module._malloc(length * 8);
+        let offset = pointer / 8;
+
+        return {
+            pointer: pointer,
+            offset: offset
+        };
+    }
+
+    static GetFloat64ArrayFromPointerWithOffset(pointer_with_offset, length) {
+        return new Float64Array(WorldWeb.module.HEAPF64.subarray(pointer_with_offset.offset, pointer_with_offset.offset + length));
     }
 }
